2100246a515f56b2595d5a67c0b0e84d4ab850f4
AlpLee <Alp@AlpLeetekiMacBook-Pro.local>
2012-12-23 12:01:24 +0800

    Initial Commit

---
+++ 计算器/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  计算器
+//
+//  Created by AlpLee on 12-12-23.
+//  Copyright (c) 2012年 AlpLee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
